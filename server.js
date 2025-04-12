0eeaab0d7b190bc48ea2f3a468700d965ac0ac45
Srimathi <srimathi.b2022ai-ml@sece.ac.in>
2025-04-12 21:45:22 +0530

    initial setup done

---
+++ server.js
@@ -0,0 +1,32 @@
+
+require('dotenv').config()
+
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+// EJS settings
+app.set('view engine', 'ejs')
+const path = require('path')
+app.set('views', path.join(__dirname, 'views'))
+app.set('layout', 'layouts/layout')
+
+// Middleware
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use('/', indexRouter)
+
+const mongoose=require('mongoose')
+mongoose.connect(process.env.DATABASE_URL)
+const db=mongoose.connection
+db.on('error',error => console.error(error))
+db.once('open',() => console.log('Connected to Mongoose'))
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+})
